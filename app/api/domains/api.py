41570727b29a3e646cc046c02deb5c9292dc5c38
kaupec <7bsanur@gmail.com>
2022-06-16 17:57:42 +0700

    mods_readable from cs0su

--- app/api/domains/api.py
+++ app/api/domains/api.py
@@ -396,8 +396,11 @@ async def api_get_player_scores(
 
     # fetch & return info from sql
     for row in rows:
+        mods = Mods(row["mods"])
         bmap = await Beatmap.from_md5(row.pop("map_md5"))
         row["beatmap"] = bmap.as_dict if bmap else None
+        row["beatmap"]["diff_mods"] = bmap.calc_star_rating(mods)
+        row["mods_readable"] = mods.__repr__()
 
     player_info = {
         "id": player.id,
